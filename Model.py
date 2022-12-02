abdab87d5755cec549149c0948ec3fd12801e32b
ali01 <ali.01119544719@gmail.com>
2022-12-02 20:56:38 +0200

    finished.

--- Model.py
+++ Model.py
@@ -41,6 +41,7 @@ def train_test_split(data):
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
+
 def tanh(x):
     return (1 - math.exp(-x)) / (1 + math.exp(-x))
 
@@ -65,7 +66,8 @@ class DeepModel:
         self.epoch = epoch
 
         self.train_data, self.test_data = train_test_split(data)
-
+        self.train_data = self.train_data.sample(frac=1).reset_index(drop=True)
+        self.test_data = self.test_data.sample(frac=1).reset_index(drop=True)
         self.x_train = self.train_data[self.train_data.columns[1:]]
         self.y_train = self.train_data[self.train_data.columns[0]]
 
@@ -101,13 +103,35 @@ class DeepModel:
             self.weights_arr.append(np.random.rand(3, num_of_neurons[num_layers - 1]))
         self.bias = bias
 
-        f = self.Forward_Feet(0)
-        s = self.Backward_Feet(f, 0)
-        self.update_weights(s, f, 0)
+    def Train(self):
+        acc = 0
+        for i in range(self.epoch):
+            for j in range(len(self.x_train)):
+                f_nets = self.Forward_Feet(self.x_train.values[j])
+                y = f_nets[-1]
+                y_index = -1
+                if y[0] > y[1] and y[0] > y[2]:
+                    y_index = 0
+                elif y[1] > y[0] and y[1] > y[2]:
+                    y_index = 1
+                elif y[2] > y[1] and y[2] > y[0]:
+                    y_index = 2
 
-    def Forward_Feet(self, index):
+                if self.y_train.values[j] == 0 and y_index == 0:
+                    acc += 1
+                elif self.y_train.values[j] == 0.5 and y_index == 1:
+                    acc += 1
+                elif self.y_train.values[j] == 1 and y_index == 2:
+                    acc += 1
+
+                sigmas = self.Backward_Feet(f_nets, self.y_train.values[j])
+                self.Update_Weights(sigmas, f_nets, self.x_train.values[j])
+            acc /= len(self.x_train)
+            print(f'train accuracy in epoch {i} = {acc}')
+
+    def Forward_Feet(self, row_data):
         f_values = []
-        input = [self.x_train.values[index]]
+        input = [row_data]
         for i in range(self.num_layers):
             layer_values = []
             for j in range(self.num_of_neurons[i]):
@@ -129,19 +153,19 @@ class DeepModel:
         f_values.append(layer_values)
         return f_values
 
-    def Backward_Feet(self, outs, index):
+    def Backward_Feet(self, f_nets, y):
         output = [0, 0, 0]
-        if self.y_test.values[index] == 0:
+        if y == 0:
             output = [1, 0, 0]
-        elif self.y_test.values[index] == 0.5:
+        elif y == 0.5:
             output = [0, 1, 0]
-        elif self.y_test.values[index] == 1:
+        elif y == 1:
             output = [0, 0, 1]
 
         sigma_arr = []
         sigma = []
         for i in range(3):
-            sigma.append((output[i] - outs[-1][i]) * self.Gradient(outs[-1][i]))
+            sigma.append((output[i] - f_nets[-1][i]) * self.Gradient(f_nets[-1][i]))
         sigma_arr.insert(0, sigma)
 
         for i in reversed(range(self.num_layers)):
@@ -150,16 +174,16 @@ class DeepModel:
                 s = 0
                 for k in range(len(sigma_arr[0])):
                     s += self.weights_arr[i + 1][k][j] * sigma_arr[0][k]
-                sigma.append(s * self.Gradient(outs[i][j]))
+                sigma.append(s * self.Gradient(f_nets[i][j]))
             sigma_arr.insert(0, sigma)
         return sigma_arr
 
-    def update_weights(self, sigmas, outs, index):
-        outs.insert(0, self.x_test.values[index])
+    def Update_Weights(self, sigmas, f_nets, row_data):
+        f_nets.insert(0, row_data)
         for i in range(len(self.weights_arr)):
             for j in range(len(self.weights_arr[i])):
                 for k in range(len(self.weights_arr[i][j])):
-                    self.weights_arr[i][j][k] += self.eta * sigmas[i][j] * outs[i][j]
+                    self.weights_arr[i][j][k] += self.eta * sigmas[i][j] * f_nets[i][k]
 
     def Gradient(self, x):
         if self.activation_function == sigmoid:
@@ -167,6 +191,65 @@ class DeepModel:
         else:
             return 1 - (x * x)
 
+    def Test(self):
+        accuracy = 0
+        confusion_matrix = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+        y_index = -1
+        t_index = -1
+        for i in range(len(self.x_test)):
+            f_nets = self.Forward_Feet(self.x_train.values[i])
+            y = f_nets[-1]
+            t = [0, 0, 0]
+
+            if y[0] > y[1] and y[0] > y[2]:
+                y = [1, 0, 0]
+                y_index = 0
+            elif y[1] > y[0] and y[1] > y[2]:
+                y = [0, 1, 0]
+                y_index = 1
+            elif y[2] > y[1] and y[2] > y[0]:
+                y = [0, 0, 1]
+                y_index = 2
+
+            if self.y_test.values[i] == 0:
+                t = [1, 0, 0]
+                t_index = 0
+            elif self.y_test.values[i] == 0.5:
+                t = [0, 1, 0]
+                t_index = 1
+            elif self.y_test.values[i] == 1:
+                t = [0, 0, 1]
+                t_index = 2
+
+            if t == y:
+                accuracy += 1
+
+            confusion_matrix[y_index][t_index] += 1
+
+        # calculate testing accuracy
+        accuracy = (accuracy / len(self.x_test)) * 100
+
+        print(f'Testing Accuracy: {accuracy:.2f}%')
+
+        # for i in range(3):
+        #     if tp[i] + fp[i] != 0:
+        #         precision = tp[i] / (tp[i] + fp[i])
+        #         recall = tp[i] / (tp[i] + fn[i])
+        #     else:
+        #         precision = 0
+        #         recall = 0
+        #     print(f'Testing Precision: {precision * 100:.2f}%')
+        #     print(f'Testing Recall: {recall * 100:.2f}%')
+        print(f'Confusion Matrix:')
+        print(f'\t\tone\t|\ttwo\t|\tthree')
+        print(f'one:\t{confusion_matrix[0][0]}\t|\t{confusion_matrix[0][1]}\t|\t{confusion_matrix[0][2]}\t')
+        print(f'two:\t{confusion_matrix[1][0]}\t|\t{confusion_matrix[1][1]}\t|\t{confusion_matrix[1][2]}\t')
+        print(f'three:\t{confusion_matrix[2][0]}\t|\t{confusion_matrix[2][1]}\t|\t{confusion_matrix[2][2]}\t')
+        print('--------------------------------')
     # def train(self):
     #     x = self.x_train
     #     y = self.y_train
@@ -234,8 +317,10 @@ class DeepModel:
 
 
 data = preprocess(dataset)
-m = DeepModel(data=data, num_layers=2, num_of_neurons=[1, 2], eta=0.001,
-              epoch=10, activation_function=Activation.sigmoid, bias=False)
+m = DeepModel(data=data, num_layers=2, num_of_neurons=[3, 4], eta=0.1,
+              epoch=1000, activation_function=Activation.sigmoid, bias=True)
+m.Train()
+m.Test()
 # m.train()
 # import cv2
 # train_data = pd.read_csv('mnist_train.csv')
